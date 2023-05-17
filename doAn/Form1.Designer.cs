41dba40db926f73d24367466b130c5e0430bab08
DucBac99 <ducbac1411.mta@gmail.com>
2023-05-17 17:01:59 +0700

    Add project files.

---
+++ doAn/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace doAn
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.btnTat = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBat = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(227, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đồ Án Tốt Nghiệp";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.statusLabel);
+            this.groupBox1.Controls.Add(this.btnTat);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnBat);
+            this.groupBox1.Location = new System.Drawing.Point(11, 132);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(259, 198);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Khối Chấp Hành";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(6, 153);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(89, 23);
+            this.statusLabel.TabIndex = 3;
+            this.statusLabel.Text = "Đèn đang:";
+            this.statusLabel.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // btnTat
+            // 
+            this.btnTat.Location = new System.Drawing.Point(139, 99);
+            this.btnTat.Name = "btnTat";
+            this.btnTat.Size = new System.Drawing.Size(94, 29);
+            this.btnTat.TabIndex = 2;
+            this.btnTat.Text = "Tắt Đèn";
+            this.btnTat.UseVisualStyleBackColor = true;
+            this.btnTat.Click += new System.EventHandler(this.btnTat_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Điều Khiển Led";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnBat
+            // 
+            this.btnBat.Location = new System.Drawing.Point(6, 99);
+            this.btnBat.Name = "btnBat";
+            this.btnBat.Size = new System.Drawing.Size(94, 29);
+            this.btnBat.TabIndex = 0;
+            this.btnBat.Text = "Bật Đèn";
+            this.btnBat.UseVisualStyleBackColor = true;
+            this.btnBat.Click += new System.EventHandler(this.btnBat_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(327, 132);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(284, 198);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Khối Cảm Biến";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 23);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Độ ẩm: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 23);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Nhiệt độ: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(77, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(188, 23);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Đo Nhiệt Độ Và Độ Ẩm";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(350, 23);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(222, 23);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Học Viện Kỹ Thuật Quân Sự";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(34, 23);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(156, 23);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Bộ Môn: Thông Tin";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(34, 358);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(548, 23);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "Đề Tài: Nghiên Cứu, Khai Thác Bộ Network Stack Cho Hệ Vi Điều Khiển";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(170, 381);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(308, 23);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "Phục Vụ Các Bài Toán IoT Và IoT Cloud";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(34, 419);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(444, 23);
+            this.label10.TabIndex = 7;
+            this.label10.Text = "Người Thực Hiện: Nguyễn Đức Bắc, Nguyễn Quốc Khánh";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(636, 451);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "Form1";
+            this.Text = "Form 1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Label label2;
+        private Button btnBat;
+        private Button btnTat;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label statusLabel;
+    }
+}
---
+++ doAn/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Windows.Forms;
+using MQTTnet;
+using MQTTnet.Client;
+namespace doAn
+
+{
+    public partial class Form1 : Form
+    {
+        private MqttClient mqttClient;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // tạo mqtt client
+            mqttClient = new MqttClient();
+
+            //kết nối và subcrice vào topic 
+            mqttClient.Connect();
+            mqttClient.Subscribe("stm32h755/test");
+
+            //Đăng ký sự kiện nhận dữ liệu từ AWS IOT CORE
+            
+        }
+
+        private void Form1_Closing(object sender, EventArgs e)
+        {
+            // ngắt kết nối mqtt
+            mqttClient.Disconnect();
+        }
+
+        private void MqttClient_MessageReceived(string topic, string message)
+        {
+            // Xử lý dữ liệu nhận được từ AWS IoT Core
+            if (topic == "your/topic")
+            {
+                if (message == "1")
+                {
+                    // Đèn được bật
+                    UpdateStatusLabel("Đèn đang bật");
+                }
+                else if (message == "0")
+                {
+                    // Đèn được tắt
+                    UpdateStatusLabel("Đèn đã tắt");
+                }
+            }
+        }
+
+        private void UpdateStatusLabel(string status)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(() => { statusLabel.Text = status; }));
+            }
+            else
+            {
+                statusLabel.Text = status;
+            }
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBat_Click(object sender, EventArgs e)
+        {
+            // gửi tin nhắn bật đèn lên aws iot core
+            mqttClient.Publish("stm32h755/test", "1");
+        }
+
+        private void btnTat_Click(object sender, EventArgs e)
+        {
+            // gửi tin nhắn bật đèn lên aws iot core
+            mqttClient.Publish("stm32h755/test", "1");
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
